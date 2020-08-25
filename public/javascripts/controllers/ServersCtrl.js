ec2d7f9d2b8dbd44e15e30be3ca52cd00a4d9846
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-08-25 15:02:29 -0500

    I've created the XML file to store the data on each server. I want to be able to dynamically wait for the XML to be converted for when the file gets larger. Potentially make them read, convert and ping one at a time async.

--- public/javascripts/controllers/ServersCtrl.js
+++ public/javascripts/controllers/ServersCtrl.js
@@ -1,28 +1,62 @@
-angular.module('ServersCtrl', []).controller('ServersController', function($scope) {
-    $scope.servers = [
-        {
-            name: 'Mas-90',
-            ipAddress: '10.1.0.91',
-            status: 'up'
-        }, {
-            name: 'Master Gastro',
-            ipAddress: '192.168.7.10',
-            status: 'down'
-        }, {
-            name: 'Data Drive',
-            ipAddress: '192.168.7.8',
-            status: 'Maintenance'
-        }, {
-            name: 'Test Server',
-            ipAddress: '10.1.0.5',
-            status: "up"
+angular.module('ServersCtrl', []).controller('ServersController', function($scope, $serverXmlConvert) {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState === 4 && (xhttp.status === 200 || xhttp.status === 304)) {
+            $scope.servers = $serverXmlConvert.convertXML(xhttp.responseXML.getElementsByTagName('Server')); // import xml data
+            console.log($scope.servers);
         }
-    ]
+    }
+
+    xhttp.open('GET', '../../res/data/servers.xml', true);
+    xhttp.send();
 
     $scope.$on('$viewContentLoaded', function() {
-        for (let server in $scope.servers) {
-            $scope.ping($scope.servers[server])
-        }
+        let tableBody = document.getElementById('server-info')
+
+        setTimeout(function() {
+            const statusClass = 'status-light ';
+
+            for (let i in $scope.servers) {
+                /*
+                STRUCTURE FOR TABLE INFO
+
+                <tr>
+                    <td class="server-name"> {{ server.name }} </td>
+                    <td class="status"><p class="status-light {{ server.status }}" id="{{ server.name }}"></p></td>
+                </tr>
+                */
+
+                // create and initialize the elements
+                let tableRow = document.createElement('tr');
+                let serverName = document.createElement('td');
+                serverName.className = 'server-name'
+                let status = document.createElement('td');
+                status.className = 'status';
+                let statusLight = document.createElement('p');
+
+                // insert info into elements
+                serverName.innerText = $scope.servers[i].name;
+                statusLight.className = statusClass + $scope.servers[i].status;
+                statusLight.id = $scope.servers[i].name;
+
+                // add the table row to the table on the page
+                status.appendChild(statusLight)
+                tableRow.appendChild(serverName)
+                tableRow.appendChild(status)
+                tableBody.appendChild(tableRow);
+            }
+
+            setTimeout(function(){
+                if ($scope.servers != undefined) {
+                    console.debug("DEBUG: PINGING SERVERS");
+                    for (let server in $scope.servers) {
+                        console.debug(`DEBUG: PINGING ${$scope.servers[server].name}`)
+                        $scope.ping($scope.servers[server]);
+                    }
+                }
+            }, 500)
+        }, 1000)
     });
 
     $scope.ping = function(server) {
@@ -47,6 +81,7 @@ angular.module('ServersCtrl', []).controller('ServersController', function($scop
                 ping.onreadystatechange = function () {
                     if (this.readyState === 4 && !TIMEOUT_ERROR) {
                         document.getElementById(server.name).style.backgroundColor = "greenyellow";
+                        console.debug(`DEBUG: ${server.name} is up`)
                         server.status = "up";
                     }
                 }
@@ -62,6 +97,7 @@ angular.module('ServersCtrl', []).controller('ServersController', function($scop
             if (TIMEOUT_ERROR) {
                 clearInterval(ping_loop);
                 document.getElementById(server.name).style.backgroundColor = "red";
+                console.debug(`DEBUG: ${server.name} is down`)
                 server.status = "down";
             }
         }, TIME_PERIOD);
